e7f860f8f87ac55bd1adcf281ba4055396e20a57
priyammaz <priyammazumdar1@gmail.com>
2026-03-05 16:40:00 -0600

    installation config

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+import numpy as np
+
+ext = Pybind11Extension(
+    "ctc_forced_aligner._ctc_align_cpp",
+    sources=["ctc_forced_aligner/ctc_align.cpp"],
+    include_dirs=[np.get_include()],   # ← add this
+    extra_compile_args=[
+        "-O3",
+        "-march=native",   
+        "-funroll-loops",
+        "-ffast-math",    
+        "-fopenmp",
+    ],
+    extra_link_args=["-fopenmp"],
+    language="c++",
+    cxx_std=17,
+)
+
+setup(
+    name="ctc_forced_aligner",
+    version="0.1.0",
+    description="Fast CTC forced alignment with a C++ backend",
+    packages=find_packages(),
+    ext_modules=[ext],
+    cmdclass={"build_ext": build_ext},
+    python_requires=">=3.10",
+    install_requires=[
+        "pybind11>=2.11",
+        "numpy",
+        "torch",
+        "transformers",
+        "torchaudio",
+        "librosa",
+        "pandas",
+        "tqdm",
+        "accelerate",
+        "torchcodec"
+    ],
+    entry_points={
+    "console_scripts": [
+        "ctc-align=ctc_forced_aligner.cli:main",
+        ],
+    },
+)
